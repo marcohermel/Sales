3e2c0676b2e93ff1ebd5839a5ab84f139b671e84
marcohermel <mf.hermel@gmail.com>
2018-12-27 21:58:18 -0200

    add

--- PraticalTest/Startup.cs
+++ PraticalTest/Startup.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,32 +13,33 @@ using PraticalTest.Models;
 
 namespace PraticalTest
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            var connection = @"Server=(localdb)\mssqllocaldb;Database=Sales;Trusted_Connection=True;";
-            services.AddDbContext<SalesContext>(options => options.UseSqlServer(connection));
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+      var connection = @"Server=(localdb)\mssqllocaldb;Database=Sales;Trusted_Connection=True;";
+      services.AddDbContext<SalesContext>(options => options.UseSqlServer(connection));
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      app.UseStaticFiles();
+      app.UseMvc();
+    }
+  }
 }
